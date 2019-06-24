01ba1a63bf2960b05f8c71cb5c1788fa7228d448
Genarodaniel <37519144+Genarodaniel@users.noreply.github.com>
2019-06-24 13:00:52 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,43 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Operações</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+   <div class="row justify-content-center">
+     <!-- <div class="col-md-8">
+         <div class="card">
+            <div class="card-body">
+               @if (session('status'))
+               <div class="alert alert-success" role="alert">
+                  {{ session('status') }}
+               </div>
+               @endif
             </div>
-        </div>
-    </div>
+         </div>
+      -->
+      <div class="col-md-5">
+      <div class="card">
+         <div class="card-header">Condomios</div>
+         <div class="card-body"><div class="col-md-12">
+         <button type="submit" class="btn btn-primary">Cadastrar novo</button>
+         <button type="submit" class="btn btn-primary">Listar</button>
+         </div>
+      </div></div>
+      <div class="card">
+         <div class="card-header">Utensílios</div>
+         <div class="card-body"><div class="col-md-12">
+         <button type="submit" class="btn btn-primary">Cadastrar novo</button>
+         <button type="submit" class="btn btn-primary">Listar</button>
+         </div>
+      </div></div>
+      <div class="card">
+         <div class="card-header">Horários</div>
+         <div class="card-body"><div class="col-md-12">
+         <label for="user_type" class="col-md-8 col-form-label text-md-right">Utensilios</label>
+         <select name="horarios" id ="horarios" class="form-control"></select></br>
+         <button type="submit" class="btn btn-primary">Cadastrar novo</button>
+         <button type="submit" class="btn btn-primary">Listar</button>
+         </div>
+      </div></div>
+   </div>
+   </div>
 </div>
 @endsection
